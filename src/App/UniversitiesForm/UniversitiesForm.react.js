5f0c2a9ac2ad1b8efa95ea9d677d4bdae86a774d
Aliaksei Yarmash <Aliaksei_Yarmash@epam.com>
2017-03-27 18:07:26 +0300

    new form styles

--- src/App/UniversitiesForm/UniversitiesForm.react.js
+++ src/App/UniversitiesForm/UniversitiesForm.react.js
@@ -5,12 +5,14 @@ export const UniversitiesForm = (props) => {
   return (
     <form className="query-form" onSubmit={props.handleSubmit}>
       <input
+        type="text"
         name="nameValue"
         placeholder="Name"
         value={props.numberOfGuests}
         onChange={props.handleInputChange}
       />
       <input
+        type="text"
         name="countryValue"
         placeholder="Country"
         value={props.numberOfGuests}
